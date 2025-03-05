2a6ed4e339135f54cfc2e3831284c32b240ab1de
PankajNagava <pank4social@gmail.com>
2025-03-05 17:26:02 +0530

    Routing app

---
+++ my-routing-app/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="text-center">
+      <h1>About Us</h1>
+      <p>We are building amazing React apps with routing and Bootstrap!</p>
+    </div>
+  );
+}
+
+export default About;
